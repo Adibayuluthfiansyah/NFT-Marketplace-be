9ed194f57c374faca52057ef892b294fe9e42ad6
Adibayu Luthfiansyah <adibayuluthfiansyah@gmail.com>
2026-01-07 12:09:15 +0700

    chore : update dependencies compiler

--- contracts/MarketPlaceNFT.sol
+++ contracts/MarketPlaceNFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -8,17 +8,25 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 contract MarketPlaceNFT is ERC721URIStorage, ERC721Enumerable, Ownable {
     uint256 private _nextTokenId;
-    
-    event NftMinted(uint256 indexed tokenId, address indexed owner, string tokenURI, string mediaType);
-    
-    constructor() ERC721("7ONG COLLECTION","7ONG") Ownable(msg.sender) {}
+
+    event NftMinted(
+        uint256 indexed tokenId,
+        address indexed owner,
+        string tokenURI,
+        string mediaType
+    );
+
+    constructor() ERC721("7ONG COLLECTION", "7ONG") Ownable(msg.sender) {}
 
     /**
      * @dev Create NFT dengan IPFS tokenURI
      * @param _tokenURI Format: "ipfs://QmXYZ..." (Fixed: sesuaikan dengan nama parameter)
      * @param mediaType Tipe media: "image", "audio", atau "video"
      */
-    function createToken(string memory _tokenURI, string memory mediaType) public onlyOwner returns (uint256) {
+    function createToken(
+        string memory _tokenURI,
+        string memory mediaType
+    ) public onlyOwner returns (uint256) {
         uint256 tokenId = _nextTokenId++;
         _mint(msg.sender, tokenId);
         _setTokenURI(tokenId, _tokenURI);
@@ -26,19 +34,30 @@ contract MarketPlaceNFT is ERC721URIStorage, ERC721Enumerable, Ownable {
         return tokenId;
     }
 
-    function _update(address to, uint256 tokenId, address auth) internal override(ERC721, ERC721Enumerable) returns (address) {
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    ) internal override(ERC721, ERC721Enumerable) returns (address) {
         return super._update(to, tokenId, auth);
     }
 
-    function _increaseBalance(address account, uint128 value) internal override(ERC721, ERC721Enumerable) {
+    function _increaseBalance(
+        address account,
+        uint128 value
+    ) internal override(ERC721, ERC721Enumerable) {
         super._increaseBalance(account, value);
     }
 
-    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
     }
 
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721URIStorage, ERC721Enumerable) returns (bool) {
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721URIStorage, ERC721Enumerable) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 }
--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity ^0.8.28;
 
 // import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
 
-error PriceNotMet (uint256 limit, uint256 sent);
+error PriceNotMet(uint256 limit, uint256 sent);
 error ItemNotForSale(address nftContract, uint256 tokenId);
 error NotListed(address nftContract, uint256 tokenId);
 error AlreadyListed(address nftContract, uint256 tokenId);
@@ -19,146 +19,164 @@ error CannotBuyOwnItem();
 error NotApprovedForMarketplace();
 error EmergencyWithdrawFailed();
 
+contract NFTMarketplace is ReentrancyGuard, Ownable, Pausable {
+    struct Listing {
+        uint256 price;
+        address seller;
+    }
+    mapping(address => mapping(uint256 => Listing)) public listings;
+    mapping(address => uint256) public proceeds;
+    uint256 public feePercent;
 
-contract NFTMarketplace is ReentrancyGuard,Ownable, Pausable {
-  struct Listing {
-    uint256 price;
-    address seller;
-  }
-  mapping(address => mapping(uint256 => Listing)) public listings;
-  mapping(address => uint256) public proceeds;
-  uint256 public feePercent;
+    // Events listed here
+    event ItemListed(
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        uint256 price,
+        address seller
+    );
 
-  // Events listed here
-  event ItemListed(
-    address indexed nftContract,
-    uint256 indexed tokenId,
-    uint256 price,
-    address seller
-  );
+    // Event Cancelled here
+    event ItemCancelled(
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address indexed seller
+    );
 
-  // Event Cancelled here
-  event ItemCancelled(
-    address indexed nftContract,
-    uint256 indexed tokenId,
-    address indexed seller
-  );
+    // Events bought here
+    event ItemBought(
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        uint256 price,
+        address buyer
+    );
 
-  // Events bought here
-  event ItemBought(
-    address indexed nftContract,
-    uint256 indexed tokenId,
-    uint256 price,
-    address buyer
-  );
+    // Events FundsWithdrawn
+    event ProceedsWithdrawn(address indexed beneficiary, uint256 amount);
 
-  // Events FundsWithdrawn
-  event ProceedsWithdrawn(
-    address indexed beneficiary,
-    uint256 amount
-  );
+    event EmergencyWithdrawn(
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address indexed to
+    );
 
-  event EmergencyWithdrawn(
-    address indexed nftContract,
-    uint256 indexed tokenId,
-    address indexed to
-  );
-
-   // getter for feePercent Deployment
-   constructor(uint256 _feePercent) Ownable (msg.sender) {
-    if(_feePercent > 20) revert FeeTooHigh();
-    feePercent = _feePercent;
-   }
-
-   //function list item
-   function listItem(address _nftContract, uint256 _tokenId, uint256 _price) external nonReentrant whenNotPaused {
-      if (_price == 0) revert PriceMustBeAboveZero();
-      if(listings[_nftContract][_tokenId].price > 0) revert AlreadyListed(_nftContract,_tokenId);
-      IERC721 nft = IERC721(_nftContract);
-      if(nft.ownerOf(_tokenId) != msg.sender) revert NotOwner();
-      if(!nft.isApprovedForAll(msg.sender,address(this))) revert NotApprovedForMarketplace();
-      listings[_nftContract][_tokenId] = Listing(_price,msg.sender);
-      emit ItemListed(_nftContract,_tokenId,_price,msg.sender);
-      nft.transferFrom(msg.sender,address(this),_tokenId);
-   }
-
-   // function update listing
-    function updateListing(address _nftContract, uint256 _tokenId, uint256 _newPrice) external nonReentrant whenNotPaused {
-    Listing storage listedItem = listings[_nftContract][_tokenId];
-    if(listedItem.seller != msg.sender) revert NotOwner();
-    if (_newPrice == 0) revert PriceMustBeAboveZero();
-    listedItem.price = _newPrice;
-    emit ItemListed(_nftContract,_tokenId,_newPrice,msg.sender);
+    // getter for feePercent Deployment
+    constructor(uint256 _feePercent) Ownable(msg.sender) {
+        if (_feePercent > 20) revert FeeTooHigh();
+        feePercent = _feePercent;
     }
 
-  // function cancel listing
-  function cancelListing(address _nftContract, uint256 _tokenId) external nonReentrant{
-    Listing memory listedItem = listings[_nftContract][_tokenId];
-    if(listedItem.price == 0) revert NotListed(_nftContract,_tokenId);
-    if(listedItem.seller != msg.sender) revert NotOwner();
-    delete(listings[_nftContract][_tokenId]);
-    emit ItemCancelled(_nftContract,_tokenId,msg.sender);
-    IERC721(_nftContract).transferFrom(address(this),msg.sender,_tokenId);
-  }
-
-   //function buy item
-   function buyItem(address _nftContract, uint256 _tokenId) external payable nonReentrant whenNotPaused {
-      Listing memory listedItem = listings[_nftContract][_tokenId];
-      if(listedItem.price == 0) revert NotListed(_nftContract,_tokenId);
-      if(msg.value < listedItem.price) revert PriceNotMet(listedItem.price,msg.value);
-      if(listedItem.seller == msg.sender) revert CannotBuyOwnItem();
-      uint256 excessAmount = msg.value - listedItem.price;
-
-      delete(listings[_nftContract][_tokenId]);
-
-      uint256 feeAmount = (listedItem.price * feePercent) / 100;
-      uint256 sellerProceeds = listedItem.price - feeAmount;
-      proceeds[listedItem.seller] += sellerProceeds;
-      proceeds[owner()] += feeAmount;
-      emit ItemBought(_nftContract,_tokenId,listedItem.price,msg.sender);
-      IERC721(_nftContract).transferFrom(address(this),msg.sender,_tokenId);
-      if(excessAmount > 0) {
-        (bool success, ) = payable(msg.sender).call{value: excessAmount}("");
-        require(success, "Refund failed");
-      }
-   }
-
-   //function withdraw proccess
-  function withdrawProceeds() external nonReentrant whenNotPaused {
-    uint256 amount = proceeds[msg.sender];
-    if (amount == 0) revert NoProceeds();
-    proceeds[msg.sender] = 0;
-
-    (bool success, ) = payable(msg.sender).call{value: amount}("");
-    require(success, "Transfer failed");
-    emit ProceedsWithdrawn(msg.sender, amount);
-  }
-  
-  /**
- * @dev Emergency function untuk rescue NFT yang terjebak di kontrak ini.
- * Hanya bisa dipanggil oleh owner sebagai last resort
- */
-  function EmergencyWithdrawNft(address _nftContract, uint256 _tokenId, address _to ) external onlyOwner nonReentrant {
-    delete(listings[_nftContract][_tokenId]);
-    try IERC721 (_nftContract).transferFrom(address(this),_to,_tokenId) {
-      emit EmergencyWithdrawn(_nftContract,_tokenId,_to);
-    }catch {
-      revert EmergencyWithdrawFailed();
+    //function list item
+    function listItem(
+        address _nftContract,
+        uint256 _tokenId,
+        uint256 _price
+    ) external nonReentrant whenNotPaused {
+        if (_price == 0) revert PriceMustBeAboveZero();
+        if (listings[_nftContract][_tokenId].price > 0)
+            revert AlreadyListed(_nftContract, _tokenId);
+        IERC721 nft = IERC721(_nftContract);
+        if (nft.ownerOf(_tokenId) != msg.sender) revert NotOwner();
+        if (!nft.isApprovedForAll(msg.sender, address(this)))
+            revert NotApprovedForMarketplace();
+        listings[_nftContract][_tokenId] = Listing(_price, msg.sender);
+        emit ItemListed(_nftContract, _tokenId, _price, msg.sender);
+        nft.transferFrom(msg.sender, address(this), _tokenId);
     }
-  }
 
-  //function to update fee percent
-  function updateFeePercent(uint256 _newFeePercent) external onlyOwner {
-    if(_newFeePercent > 20) revert FeeTooHigh();
-    feePercent = _newFeePercent;
-  }
+    // function update listing
+    function updateListing(
+        address _nftContract,
+        uint256 _tokenId,
+        uint256 _newPrice
+    ) external nonReentrant whenNotPaused {
+        Listing storage listedItem = listings[_nftContract][_tokenId];
+        if (listedItem.seller != msg.sender) revert NotOwner();
+        if (_newPrice == 0) revert PriceMustBeAboveZero();
+        listedItem.price = _newPrice;
+        emit ItemListed(_nftContract, _tokenId, _newPrice, msg.sender);
+    }
 
-  //function pause and unpause
-  function pause() external onlyOwner {
-    _pause();
-  }
-  function unpause() external onlyOwner {
-    _unpause();
-  }
+    // function cancel listing
+    function cancelListing(
+        address _nftContract,
+        uint256 _tokenId
+    ) external nonReentrant {
+        Listing memory listedItem = listings[_nftContract][_tokenId];
+        if (listedItem.price == 0) revert NotListed(_nftContract, _tokenId);
+        if (listedItem.seller != msg.sender) revert NotOwner();
+        delete (listings[_nftContract][_tokenId]);
+        emit ItemCancelled(_nftContract, _tokenId, msg.sender);
+        IERC721(_nftContract).transferFrom(address(this), msg.sender, _tokenId);
+    }
+
+    //function buy item
+    function buyItem(
+        address _nftContract,
+        uint256 _tokenId
+    ) external payable nonReentrant whenNotPaused {
+        Listing memory listedItem = listings[_nftContract][_tokenId];
+        if (listedItem.price == 0) revert NotListed(_nftContract, _tokenId);
+        if (msg.value < listedItem.price)
+            revert PriceNotMet(listedItem.price, msg.value);
+        if (listedItem.seller == msg.sender) revert CannotBuyOwnItem();
+        uint256 excessAmount = msg.value - listedItem.price;
+
+        delete (listings[_nftContract][_tokenId]);
+
+        uint256 feeAmount = (listedItem.price * feePercent) / 100;
+        uint256 sellerProceeds = listedItem.price - feeAmount;
+        proceeds[listedItem.seller] += sellerProceeds;
+        proceeds[owner()] += feeAmount;
+        emit ItemBought(_nftContract, _tokenId, listedItem.price, msg.sender);
+        IERC721(_nftContract).transferFrom(address(this), msg.sender, _tokenId);
+        if (excessAmount > 0) {
+            (bool success, ) = payable(msg.sender).call{value: excessAmount}(
+                ""
+            );
+            require(success, "Refund failed");
+        }
+    }
+
+    //function withdraw proccess
+    function withdrawProceeds() external nonReentrant whenNotPaused {
+        uint256 amount = proceeds[msg.sender];
+        if (amount == 0) revert NoProceeds();
+        proceeds[msg.sender] = 0;
+
+        (bool success, ) = payable(msg.sender).call{value: amount}("");
+        require(success, "Transfer failed");
+        emit ProceedsWithdrawn(msg.sender, amount);
+    }
+
+    /**
+     * @dev Emergency function untuk rescue NFT yang terjebak di kontrak ini.
+     * Hanya bisa dipanggil oleh owner sebagai last resort
+     */
+    function EmergencyWithdrawNft(
+        address _nftContract,
+        uint256 _tokenId,
+        address _to
+    ) external onlyOwner nonReentrant {
+        delete (listings[_nftContract][_tokenId]);
+        try IERC721(_nftContract).transferFrom(address(this), _to, _tokenId) {
+            emit EmergencyWithdrawn(_nftContract, _tokenId, _to);
+        } catch {
+            revert EmergencyWithdrawFailed();
+        }
+    }
+
+    //function to update fee percent
+    function updateFeePercent(uint256 _newFeePercent) external onlyOwner {
+        if (_newFeePercent > 20) revert FeeTooHigh();
+        feePercent = _newFeePercent;
+    }
+
+    //function pause and unpause
+    function pause() external onlyOwner {
+        _pause();
+    }
+    function unpause() external onlyOwner {
+        _unpause();
+    }
 }
-
